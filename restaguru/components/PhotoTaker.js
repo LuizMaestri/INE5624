85969743e58dc5015b295b65037bd1f7daf76d32
LuizMaestri <luizricardomaestri@gmail.com>
2017-06-23 12:27:02 -0300

    add ImagePicker

---
+++ restaguru/components/PhotoTaker.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import { Button, Icon } from 'native-base';
+import ImagePicker from 'react-native-image-picker';
+
+export default class PhotoTaker extends Component {
+    constructor(){
+        super();
+        this.state = {
+            photo: null
+        };
+        this.picker = this.picker.bind(this);
+    }
+
+    picker() {
+        var options = {
+            title: 'Select Avatar',
+            storageOptions: {
+                skipBackup: true,
+                path: 'images'
+            }
+        };
+
+        /**
+         * The first arg is the options object for customization (it can also be null or omitted for default options),
+         * The second arg is the callback which sends object: response (more info below in README)
+         */
+        ImagePicker.showImagePicker(options, (response) => {
+            console.log('Response = ', response);
+
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            }
+            else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            }
+            else {
+                // let source = { uri: response.uri };
+
+                // You can also display the image using data:
+                let photo = { uri: 'data:image/jpeg;base64,' + response.data };
+
+                this.setState({photo});
+            }
+        });
+    }
+
+    render(){
+        if(!this.state.photo){
+            return (
+                <Button small onPress={ this.picker }>
+                    <Icon name="camera"/>
+                </Button>
+            );
+        }
+        return (<Image source={ this.state.photo }/>);
+    }
+}
