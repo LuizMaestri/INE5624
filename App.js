dd4ab0f2baaf8bb99c8a4b4dad5a1a2410424fc8
LuizMaestri <luizricardomaestri@gmail.com>
2017-06-24 16:01:37 -0300

    fix Conflicts

--- App.js
+++ App.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-import Expo from 'expo';
-import React, { Component } from 'react';
-import { StyleProvider } from 'native-base'; 
-import Application from './restaguru/components/Application';
-import getTheme from './restaguru/theme/components';
-
-export default class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      isLoading: false,
-      isReady: false,
-      theme: require('./restaguru/theme/variables/restaguru').default
-    };
-    this.changeTheme = this.changeTheme.bind(this);
-  }
-
-  changeTheme(theme){
-    console.log(this);
-    this.setState({ theme });
-  }
-
-  async componentWillMount() {
-    await Expo.Font.loadAsync({
-      'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-      'Ionicons': require('native-base/Fonts/Ionicons.ttf'),
-    });
-
-    this.setState({isReady: true});
-  }
-
-  render() {
-    if (!this.state.isReady) {
-      return <Expo.AppLoading />;
-    }
-    {}
-    return (
-      <StyleProvider style={getTheme(this.state.theme)}>
-        <Application onChangeTheme={ this.changeTheme } theme={this.state.theme}/>
-      </StyleProvider>
-    );
-  }
-}
-
-=======
 import React, { Component } from 'react';
 import { StyleProvider } from 'native-base'; 
 import Application from './restaguru/components/Application';
@@ -80,4 +32,3 @@ export default class App extends Component {
   }
 }
 
->>>>>>> remove_expo
