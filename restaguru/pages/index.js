343c35f1d8b0854e8098c1beffaa1225e6a25ad4
LuizMaestri <luizricardomaestri@gmail.com>
2017-06-26 16:11:26 -0300

    add pages to index

--- restaguru/pages/index.js
+++ restaguru/pages/index.js
@@ -1,5 +1,7 @@
 import Profile from './Profile';
 import Home from './Home';
 import Add from './Add';
+import AdvanceSearch from './Search';
+import RestaurantPage from './Restaurant';
 
-export { Profile, Home, Add };
+export { Profile, Home, Add, AdvanceSearch, RestaurantPage };
