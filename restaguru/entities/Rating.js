79ae08234e31831a66433e4e1498dc2833bd8be8
LuizMaestri <luizricardomaestri@gmail.com>
2017-06-29 15:35:23 -0300

    Generate auto increment id

--- restaguru/entities/Rating.js
+++ restaguru/entities/Rating.js
@@ -1,5 +1,6 @@
 export default class Rating {
     constructor(user, price=50, food=50, satisfaction=50, atmosphere=50, comment='', privateComment=''){
+        this.id = Rating.generateId();
         this.user = user;
         this.price = price;
         this.food = food;
@@ -8,4 +9,12 @@ export default class Rating {
         this.comment = comment;
         this.privateComment = privateComment;
     }
+
+    static generateId(){
+        let thisId = id;
+        id++;
+        return thisId;
+    }
 }
+
+var id = 1;
