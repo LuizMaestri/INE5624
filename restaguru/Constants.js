ff5483deedf9666edabad28fb592995457c043ca
LuizMaestri <luizricardomaestri@gmail.com>
2017-06-30 18:00:36 -0300

    translate to english

--- restaguru/Constants.js
+++ restaguru/Constants.js
@@ -1,4 +1,4 @@
 import { Guru, Address } from './entities';
 
-export const user = new Guru('Você', new Address('Brazil', 'Florianópolis'), 22);
+export const user = new Guru('You', new Address('Brazil', 'Florianópolis'), 22);
 export const imageDefault = require('./img/default.png');
