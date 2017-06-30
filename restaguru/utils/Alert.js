4d79571e51a5e001ee7695ed14fe99f385acd1ea
LuizMaestri <luizricardomaestri@gmail.com>
2017-06-30 15:52:10 -0300

    add action log on alert

--- restaguru/utils/Alert.js
+++ restaguru/utils/Alert.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Alert } from 'react-native';
+import { saveLog } from './Log'
 
 export const okAlert = (title, msg) =>{
     Alert.alert(
-    title,
-    msg,
-    [{text: 'OK', onPress: () => console.log('OK Pressed')}]
+        title,
+        msg,
+        [
+            {
+                text: 'OK', onPress: () => {
+                    let log = {
+                        action: 'Alert OK Pressed',
+                        date: new Date().toString()
+                    }
+                    saveLog(log);
+                }
+            }
+        ]
     )
 }
